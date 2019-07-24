2b162b36174818fceaaa6c5710f49ecd17aa2848
JubeatWwW <l6104400@gmail.com>
2019-07-24 16:39:06 +0800

    feat: 🎸 Add time accumulator
    Add functions that calculate the total time user stop on a single page

--- src/background.ts
+++ src/background.ts
@@ -3,12 +3,17 @@ type Sender = chrome.runtime.MessageSender;
 
 interface Message {
     action: string;
-    data?: MessageNavigateData;
+    data?: MessageNavigateData | MessageTimeData;
 };
 
 interface MessageNavigateData {
     location: Location;
 };
+
+interface MessageTimeData {
+    location: Location;
+    time: number;
+}
 
 const getSyncStorage = keys => new Promise(resolv => {
     chrome.storage.sync.get(keys, (result) => {
@@ -31,10 +36,10 @@ const navigateHandler = async (data: MessageNavigateData, tabId: number) => {
         if (result[origin][pathname]) {
             result[origin][pathname].count += 1;
         } else {
-            result[origin][pathname] = { count: 0 };
+            result[origin][pathname] = { count: 0, time: 0 };
         }
     } else {
-        result[origin] = { [pathname]: { count: 0 }, count: 0 };
+        result[origin] = { [pathname]: { count: 0, time: 0 }, count: 0, time: 0 };
     }
     const count = result[origin][pathname].count;
 
@@ -49,10 +54,30 @@ const navigateHandler = async (data: MessageNavigateData, tabId: number) => {
     }
 }
 
-chrome.runtime.onMessage.addListener(async (msg: Message, sender: Sender) => {
+const accumulateTime = async (data: MessageTimeData) => {
+    const { time, location: { origin, pathname } } = data;
+    const result = await getSyncStorage(origin);
+
+    result[origin].time += time;
+    result[origin][pathname].time += time;
+    await setSyncStorage({ [origin]: result[origin] });
+}
+
+chrome.runtime.onMessage.addListener((msg: Message, sender: Sender) => {
     switch (msg.action) {
         case 'NAVIGATE':
-            navigateHandler(msg.data, sender.tab.id);
+            navigateHandler(<MessageNavigateData>msg.data, sender.tab.id);
+            break;
+        case 'FOCUS':
+            console.log('focus', msg);
+            break;
+        case 'BLUR':
+            console.log('blur', msg);
+            accumulateTime(<MessageTimeData>msg.data);
+            break;
+        case 'LEAVE':
+            console.log('leave', msg);
+            accumulateTime(<MessageTimeData>msg.data);
             break;
         default:
             break;
--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,3 +1,31 @@
+let time = 0;
+let timeInterval = 0;
+
 document.addEventListener('DOMContentLoaded', () => {
     chrome.runtime.sendMessage({ action: 'NAVIGATE', data: { location } });
+    timeInterval = window.setInterval(() => {
+        time += 1;
+    }, 1000);
+
+    window.onfocus = () => {
+        chrome.runtime.sendMessage({ action: 'FOCUS', data: { location } });
+        timeInterval = window.setInterval(() => {
+            time += 1;
+        }, 1000);
+    };
+
+    window.onblur = () => {
+        chrome.runtime.sendMessage({ action: 'BLUR', data: { location, time } });
+        clearInterval(timeInterval);
+        time = 0;
+    };
+
 });
+
+window.onbeforeunload = () => {
+    chrome.runtime.sendMessage({ action: 'LEAVE', data: { location, time } });
+    clearInterval(timeInterval);
+    time = 0;
+
+    return null;
+};
